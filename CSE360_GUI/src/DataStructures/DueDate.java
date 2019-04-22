c7fdb6a900e855b840a5816dd0c684672c7c7dbb
Trevor Angle <tangle.angle2@gmail.com>
2019-04-22 16:34:08 -0700

    Merged changes

--- CSE360_GUI/src/DataStructures/DueDate.java
+++ CSE360_GUI/src/DataStructures/DueDate.java
@@ -1,11 +1,14 @@
 package DataStructures;
 
 public class DueDate implements Comparable<DueDate> {
-
+	
+	private int month;
+	private int day;
+	private int year;
+	
 	@Override
 	public int compareTo(DueDate o) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
-
 }
--- CSE360_GUI/src/Status.java
+++
@@ -1,5 +0,0 @@
-import java.util.*;
-
-public enum Status{
-NotStarted ("Not Started"), InProgress("In Progress"), Finished("Finished");
-}
