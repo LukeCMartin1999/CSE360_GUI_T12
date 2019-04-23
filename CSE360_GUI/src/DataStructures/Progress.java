bd818eb58dc8e1dca60258c168953579b75729bc
asivaku7 <46466790+asivaku7@users.noreply.github.com>
2019-04-23 05:10:52 -0700

    Update Progress.java

--- CSE360_GUI/src/DataStructures/Progress.java
+++ CSE360_GUI/src/DataStructures/Progress.java
@@ -1,10 +1,26 @@
 package DataStructures;
+import java.util.*;
 
 public class Progress {
 	public enum Status { NOT_STARTED, IN_PROGRESS, FINISHED };
 	
-	public Status getStatus() {
+	public Status objectdefine() {
 		//TODO
+		Status s1 = Status.NOT_STARTED;
+		Status s2 = Status.IN_PROGRESS;
+		Status s3 = Status.FINISHED;
 		return null;
 	}
+	public Status checkingDate()
+	{
+		if(s2 || s3)
+		{
+			Date newdate = new Date() 
+		}
+	}
+	public Status getStatus()
+	{
+		return null;
+	}
+		
 }
