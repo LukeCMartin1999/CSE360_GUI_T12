3adc99f3d595ccc8769e8b8662132c561c156540
Trevor Angle <tangle.angle2@gmail.com>
2019-04-25 18:31:08 -0700

    Removed unused import

--- CSE360_GUI/src/mainMenu.java
+++ CSE360_GUI/src/mainMenu.java
@@ -12,7 +12,6 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 
-import DataStructures.Entry;
 import DataStructures.ToDoList;;
 
 
